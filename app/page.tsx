aefff86bab3766798d8396ae3fd0fabe25301873
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2023-10-25 11:09:03 +0530

    authenticaion setup using next-auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
 import { db } from "@/lib/db";
+import Button from "./components/ui/Button";
 
 export default async function Home() {
-  await db.set("hello", "hello");
-
-  return (
-    <div className="text-red-500">
-      <h1>Hello World!</h1>
-    </div>
-  );
+  return <Button variant={"ghost"}>Hello</Button>;
 }
