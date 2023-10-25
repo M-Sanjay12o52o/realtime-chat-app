d849a97f626cf3b4d9a2f2b162041bb0bbcb63d5
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2023-10-25 12:23:24 +0530

    addfriend added, login not working

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
-import { db } from "@/lib/db";
 import Button from "./components/ui/Button";
+import Link from "next/link";
 
 export default async function Home() {
-  return <Button variant={"ghost"}>Hello</Button>;
+  return (
+    <div>
+      <Button>
+        <Link href="/login">Login</Link>
+      </Button>
+      <Button variant={"ghost"}>Hello</Button>;
+    </div>
+  );
 }
