919ac29109eae4ed44b969d0b0bab1cb0d540862
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2023-10-25 10:29:22 +0530

    button comp created with class variance, clsx & tailwind-merge

---
+++ app/components/ui/Button.tsx
@@ -0,0 +1,56 @@
+import { cn } from "@/lib/utils";
+import { VariantProps, cva } from "class-variance-authority";
+import { Loader2 } from "lucide-react";
+import { ButtonHTMLAttributes, FC } from "react";
+
+// creating variance of button using class-variance-authority package
+const buttonVariants = cva(
+  "active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-md transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none",
+  {
+    variants: {
+      variant: {
+        default: "bg-slate-900 text-white hover:bg-slate-800",
+        ghost: "bg-transparent hover:text-slate-900 hover:bg-slate-200",
+      },
+      size: {
+        default: "h-10 py-2 px-4",
+        sm: "h-9 px-2",
+        lg: "h-11 px-8",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+// this will help our Button have access to the default attributes the html button has
+// and also the properties that we've defined using cva
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  isLoading?: boolean;
+}
+
+export const Button: FC<ButtonProps> = ({
+  className,
+  children,
+  variant,
+  isLoading,
+  size,
+  ...props
+}) => {
+  return (
+    <button
+      className={cn(buttonVariants({ variant, size, className }))}
+      disabled={isLoading}
+      {...props}
+    >
+      {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : null}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
