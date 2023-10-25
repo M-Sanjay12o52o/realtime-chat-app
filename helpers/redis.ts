a67fb4872e74cc12d8f634c9f70d84230ba32411
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2023-10-25 15:25:15 +0530

    sending friend request

---
+++ helpers/redis.ts
@@ -0,0 +1,28 @@
+// checking whether ths user is in the db
+
+const upstashRedisRestUrl = process.env.UPSTASH_REDIS_REST_URL;
+const authToken = process.env.UPSTASH_REDIS_REST_TOKEN;
+
+type Command = "zrange" | "sismember" | "get" | "smembers";
+
+export async function fetchRedis(
+  command: Command,
+  ...args: (string | number)[]
+) {
+  // request to the rest url
+  const commandUrl = `${upstashRedisRestUrl}/${command}/${args.join("/")}`;
+
+  const response = await fetch(commandUrl, {
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+    },
+    cache: "no-store",
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error executing Redis command: ${response.statusText}`);
+  }
+
+  const data = await response.json();
+  return data.result;
+}
